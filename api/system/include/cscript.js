72a433f63a3e3c17ca96e0b11f8f92d3540f63c9
Берсенёв Никита Дмитриевич <bersenoff@bk.ru>
2018-12-29 16:30:44 +0300

    fix

--- api/system/include/cscript.js
+++ api/system/include/cscript.js
@@ -8,21 +8,15 @@ module.exports = async (data, bot) => {
     const cscript = spawn(`cscript ${data.text.replace('/cscript ', '')}`, [], { cwd: 'D:\\SQLReporting\\test\\VBScripts', shell: true });
 
     cscript.stdout.on('data', (data) => {
-        if (chat) {
-            bot.sendMessage(chat.id, data.toString());
-        }
+        bot.sendMessage(chat.id, data.toString());
     });
 
     cscript.stderr.on('data', (data) => {
-        if (chat) {
-            bot.sendMessage(chat.id, `Ошибка внутри скрипта: ${data.toString()}`)
-        }
+        bot.sendMessage(chat.id, `Ошибка: ${data.toString()}`)
     });
 
     cscript.on('close', (code) => {
-        if (chat) {
-            bot.sendMessage(chat.id, `Скрипт завершился с кодом ${code.toString()}`);
-        }
+        bot.sendMessage(chat.id, `Скрипт завершился с кодом ${code.toString()}`);
     });
 
 }
