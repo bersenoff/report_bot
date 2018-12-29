39579504f3c9d569f3bb9891f93ecd53efd7846c
Берсенёв Никита Дмитриевич <bersenoff@bk.ru>
2018-12-29 15:25:50 +0300

    test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 (async () => {
     const child_process = require('child_process');
 
-    const result = await child_process.execSync('ls', { cwd: 'D:\\SQLReporting\\test\\VBScripts' }).toString();
+    const result = await child_process.execSync('cscript pkOmniChat.vbs', { cwd: 'D:\\SQLReporting\\test\\VBScripts' }).toString();
 
     console.log(result);
 })();
